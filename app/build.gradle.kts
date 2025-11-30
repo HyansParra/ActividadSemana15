78e1c20a44481f1e837b5be24e39d80260d3dacf
HyansParra <hn.parravaldivia@gmail.com>
2025-11-30 19:53:49 -0300

    Configuración de dependencias necesarias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Dependencias MQTT Paho
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
+
+    // Soporte Legacy (necesario para que Paho funcione bien en versiones nuevas de Android)
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 }
